4b7e3cec39184c1d67f41d1f6cd011bdfad1b048
XGhozt <me@xghozt.com>
2018-10-10 10:27:10 -0700

    Update FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex border-b border-40" style="padding-bottom: 5px">
-        <h2>{{field.name}}</h2>
+        <h2 class="arsenaltech-nova-header">{{field.name}}</h2>
     </div>
 </template>
 
